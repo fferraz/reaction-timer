ab4cf5c43d35f68bf443725d2df3d017093ddd59
Felipe F. Morgado <ffmoliveira@ymail.com>
2023-09-13 09:55:25 +0200

    reaction time project

---
+++ src/components/Results.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="results">
+        <p> Reaction time: {{ score }} ms</p>
+        <p class="rank"> {{ rank }} </p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data(){
+        return{
+            rank: null
+        }
+    },
+    mounted(){
+        if (this.score < 250){
+            this.rank = 'Ninja Fingers'
+        } else if (this.score < 400){
+            this.rank = 'Rapid Reflexes'
+        } else {
+            this.rank = 'Snail pace...'
+        }
+    }
+}
+</script>
+
+<style>
+    .results{
+        color: red;
+    }
+    .rank{
+        font-size: 1.4em;
+        color: #0faf87;
+        font-weight: bold;
+    }
+
+</style>
